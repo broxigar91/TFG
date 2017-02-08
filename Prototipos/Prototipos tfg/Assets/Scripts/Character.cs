bfb80650ede040c2268d013bf95f298590fa64b5
broxigar91 <ichigobiar@gmail.com>
2017-02-08 18:25:56 +0100

    Algunos scripts mas (aun sin implementar)

---
+++ Prototipos/Prototipos tfg/Assets/Scripts/Character.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+
+    private string charName;
+    private int hp;
+    private int str;
+    private int def;
+    private int intelect;
+    private int mdef;
+    private int spe;
+
+    public string CharName
+    {
+        get { return charName; }
+        set { charName = value; }
+    }
+
+    public int Hp
+    {
+        get { return hp; }
+        set { hp = value; }
+    }
+
+    public int Str
+    {
+        get { return str; }
+        set { str = value; }
+    }
+
+    public int Def
+    {
+        get { return def; }
+        set { def = value; }
+    }
+
+    public int Intelect
+    {
+        get { return intelect; }
+        set { intelect = value; }
+    }
+
+    public int Spe
+    {
+        get { return spe; }
+        set { spe = value; }
+    }
+
+
+    /*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+}
---
+++ Prototipos/Prototipos tfg/Assets/Scripts/NPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    private int id;
+    private string npcName;
+
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
